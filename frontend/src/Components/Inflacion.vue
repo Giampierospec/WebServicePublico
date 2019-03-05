e8fb20e4b5f598312b23016abffb4a5cf0d1e3ea
Giampiero Specogna <giampi_12@hotmail.com>
2019-03-04 21:09:48 -0400

    Cambios para el frontend

--- frontend/src/Components/Inflacion.vue
+++ frontend/src/Components/Inflacion.vue
@@ -1,9 +1,67 @@
 <template>
-    
+      <div>
+    <div class="row">
+      <div class="col-sm-8 offset-sm-2">
+          <h4>Búsqueda de inflación por período</h4>
+          <hr>
+          <div class="input-group">
+        <div class="input-group-prepend">
+          <label for="srch" class="input-group-text"><i class="fas fa-business-time"></i></label>
+        </div>
+        <input type="number" class="form-control" placeholder="Introduzca el periodo" name="srch" v-model="srch" @keypress.enter="search">
+      </div>
+      </div>
+      
+    </div>
+    <br>
+    <div class="row" v-if="dt">
+      <div class="col-sm-8 offset-sm-2">
+         <div class="card">
+           <div class="card-content">
+              <pre>
+          <code>
+          {{dt}}
+          </code>
+        </pre>
+           </div>
+         </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import axios from "axios";
+import swal from "sweetalert";
 export default {
-    
+  data(){
+    return{
+      srch:"",
+      dt: "",
+      error: ""
+    };
+  },
+  methods:{
+    search(e){
+      if(e.target.value.length === 4)
+      {
+        e.preventDefault();
+        axios.get(`http://wspublicosapi.azurewebsites.net/api/inflacion?year=${this.srch}`)
+            .then((res)=> this.dt = res.data)
+            .catch((e)=> this.error = e.response.data);
+      }
+    }
+  },
+  watch:{
+    error(val){
+      if(val)
+        swal({
+          title:"Error",
+           text: val,
+           icon:"error"
+        });
+        this.error = "";
+    }
+  }
 }
 </script>
 
